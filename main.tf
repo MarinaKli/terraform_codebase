e4324566859b9e2f68eea34cc7b3e37ad8d160f9
MarinaKli <163959164+MarinaKli@users.noreply.github.com>
2024-08-19 21:16:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = var.ami_id
-  instance_type = var.instance_type
+   instance_type = var.instance_type
 
   tags = {
-    Name = "example-instance"
+     Name = "example-instance"
   }
 }
 
